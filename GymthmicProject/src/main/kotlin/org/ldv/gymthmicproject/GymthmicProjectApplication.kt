4ac5f08b347b0babefdb5caccb5fff8e68191f38
Kellyane DIAKUMPUNA <yauth11@gmail.com>
2025-11-10 08:55:07 +0100

    commit du projet

---
+++ GymthmicProject/src/main/kotlin/org/ldv/gymthmicproject/GymthmicProjectApplication.kt
@@ -0,0 +1,11 @@
+package org.ldv.gymthmicproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GymthmicProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<GymthmicProjectApplication>(*args)
+}
